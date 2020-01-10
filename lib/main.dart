dd36879b52c3c077688f8217627f5d6012d6487f
Matheus <mha@ecomp.poli.br>
2020-01-10 13:39:41 -0300

    Added fonts and stateless widget for Home Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,36 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text('My App'),
-      centerTitle: true,
-    ),
-    body: Center(
-      child: Text('Body Text'),
-    ),
-    floatingActionButton: FloatingActionButton(
-      child: Text('Click'),
-    ),
-  ),
+  home: Home()
 ));
+
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My App'),
+        centerTitle: true,
+        backgroundColor: Colors.blueGrey[600],
+      ),
+      body: Center(
+        child: Text(
+          'Body Text',
+          style: TextStyle(
+            fontSize: 25.0,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+            fontFamily: 'TitilliumWeb',
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text('Click'),
+        backgroundColor: Colors.pink[400],
+      ),
+    );
+  }
+}
+
